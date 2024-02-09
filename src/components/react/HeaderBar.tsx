894d4803176a63684635283051824fd4c1ff0ae6
Rajat Mhetre <mhetrerajat@gmail.com>
2024-02-09 00:50:33 -0500

    add wip label

--- src/components/react/HeaderBar.tsx
+++ src/components/react/HeaderBar.tsx
@@ -1,15 +1,19 @@
 
 'use client';
 
-import { Navbar } from 'flowbite-react';
+import { Navbar, Badge } from 'flowbite-react';
+import { FaMedapps } from "react-icons/fa6";
 
 const HeaderBar = () => {
   return (
     <Navbar fluid rounded>
       <Navbar.Brand href="/">
-        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Rajat Mhetre</span>
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+          Rajat Mhetre
+        </span>
       </Navbar.Brand>
       <Navbar.Toggle />
+      <Badge color="warning" size="xs">Work In Progress</Badge>
       <Navbar.Collapse>
         <Navbar.Link href="/" active>
           Home
