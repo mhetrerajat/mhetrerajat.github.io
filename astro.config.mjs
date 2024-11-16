3ceb1bb0392941bf5c3e49b15c233396cfd4c1fa
Rajat Mhetre <mhetrerajat@gmail.com>
2024-11-15 18:00:56 -0800

    feat: add giscus for the posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
+import react from "@astrojs/react";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://mhetrerajat.github.io",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), react()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
---
+++ src/components/Comments.tsx
@@ -0,0 +1,38 @@
+/* Reference: https://www.thomasledoux.be/blog/hosting-blog-comments-reactions-github-discussions */
+
+import * as React from "react";
+import Giscus from "@giscus/react";
+
+const id = "inject-comments";
+
+const Comments = () => {
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <div id={id}>
+      {mounted ? (
+        <Giscus
+          id={id}
+          repo="mhetrerajat/mhetrerajat.github.io"
+          repoId="MDEwOlJlcG9zaXRvcnk2MzEyNjc1Mw=="
+          category="Announcements"
+          categoryId="DIC_kwDOA8M84c4CdFPu"
+          mapping="pathname"
+          strict="0"
+          reactionsEnabled="1"
+          emitMetadata="0"
+          theme="preferred_color_scheme"
+          inputPosition="top"
+          lang="en"
+          loading="lazy"
+        />
+      ) : null}
+    </div>
+  );
+};
+
+export default Comments;
