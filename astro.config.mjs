e97db9af282c9fdc6fe180b1802a981cf85d07ba
Rajat Mhetre <mhetrerajat@gmail.com>
2024-11-15 18:17:24 -0800

    build: add playform/compress to minify assets

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,12 +7,10 @@ import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import react from "@astrojs/react";
 
-import compress from "astro-compress";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://mhetrerajat.github.io",
-  integrations: [mdx(), sitemap(), tailwind(), react(), compress()],
+  integrations: [mdx(), sitemap(), tailwind(), react(), (await import("@playform/compress")).default()],
   markdown: {
     remarkPlugins: [remarkMath],
     rehypePlugins: [rehypeKatex]
